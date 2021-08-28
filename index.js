c0d8e2c5824a32957135576d12391e4700d8c3cb
Coefficient <78182297+Module32@users.noreply.github.com>
2021-08-28 20:10:07 +0000

    MAJOR VERSION! New APIs added!

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function invert(avatar) {
+    return `https://someapi.xyz/invert?avatar=${avatar}`
+};
+
+module.exports = function wanted(avatar) {
+    return `https://someapi.xyz/invert?avatar=${avatar}`
+};
