c53c2310c5aa87c765dc3809a01f31c20416be17
Coefficient <78182297+Module32@users.noreply.github.com>
2021-08-29 19:32:26 +0000

    Exported multiple APIs like wanted and greyscale, and updated code-- more functions coming very soon

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-module.exports = async function invert(avatar) {
+async function invert(avatar) {
     return `https://someapi.xyz/invert?avatar=${avatar}`
 };
 
-module.exports = async function wanted(avatar) {
+async function wanted(avatar) {
     return `https://someapi.xyz/wanted?avatar=${avatar}`
 };
 
+async function greyscale(avatar) {
+    return `https://someapi.xyz/greyscale?avatar=${avatar}`
+};
 
+module.exports = {invert, wanted, greyscale}
