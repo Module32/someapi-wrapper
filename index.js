8a582e362d7c1ba9ad4add20395a2de6a8c7dd27
Coefficient <78182297+Module32@users.noreply.github.com>
2021-08-31 11:33:56 +0000

    New warning messages in console and theme for terminal, depending on what someapi returns

--- index.js
+++ index.js
@@ -7,65 +7,80 @@ This is the SomeAPI wrapper for npm, designed for Someapi, a website with a huge
 
 // Imports
 const axios = require('axios');
+var colors = require('colors');
+
+// COLORS -> setting theme
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
 
 // Discord and image manipulation APIs
 async function invert(avatar) {
   if (!avatar) {
-    console.log("\x1b[31m", "  > someapi input err: please provide an avatar!");
+    console.log("  > someapi input warning: please provide an avatar!".warn);
     process.exit();
   }
   try {
     return `https://someapi.xyz/invert?avatar=${avatar}`
   } catch (err) {
-    console.log("\x1b[31m", `  > someapi internal err:\n${err.stack}`);
+    console.log(`  > someapi internal err:\n${err.stack}`.error);
   }
 };
 
 async function wanted(avatar) {
   if (!avatar) {
-    console.log("\x1b[31m", "  > someapi input err: please provide an avatar!");
+    console.log("  > someapi input warning: please provide an avatar!".warn);
     process.exit();
   }
   try {
     return `https://someapi.xyz/wanted?avatar=${avatar}`
   } catch (err) {
-    console.log("\x1b[31m", `  > someapi internal err:\n${err.stack}`);
+    console.log(`  > someapi internal err:\n${err.stack}`.error);
   }
 };
 
 async function greyscale(avatar) {
   if (!avatar) {
-    console.log("\x1b[31m", "  > someapi input err: please provide an avatar!");
+    console.log("  > someapi input warning: please provide an avatar!".warn);
     process.exit();
   }
   try {
     return `https://someapi.xyz/greyscale?avatar=${avatar}`
   } catch (err) {
-    console.log("\x1b[31m", `  > someapi internal err:\n${err.stack}`);
+    console.log(`  > someapi internal err:\n${err.stack}`.error);
   }
 };
 
 async function alert(avatar) {
   if (!avatar) {
-    console.log("\x1b[31m", "  > someapi input err: please provide an avatar!");
+    console.log("  > someapi input warning: please provide an avatar!".warn);
     process.exit();
   }
   try {
     return `https://someapi.xyz/alert?avatar=${avatar}`
   } catch (err) {
-    console.log("\x1b[31m", `  > someapi internal err:\n${err.stack}`);
+    console.log(`  > someapi internal err:\n${err.stack}`.error);
   }
 };
 
 async function god(text) {
   if (!text) {
-    console.log("\x1b[31m", "  > someapi input err: please provide some text!");
+    console.log("  > someapi input warning: please provide some text!".warn);
     process.exit();
   }
   try {
     return `https://someapi.xyz/god?text=${text}`
   } catch (err) {
-    console.log("\x1b[31m", `  > someapi internal err:\n${err.stack}`);
+    console.log(`  > someapi internal err:\n${err.stack}`.error);
   }
 };
 
@@ -76,13 +91,13 @@ async function dogfacts() {
     const dataPromise = promise.then((response) => response.data.fact);
     return dataPromise;
   } catch (err) {
-    console.log("\x1b[31m", `  > someapi internal err:\n${err.stack}`);
+    console.log(`  > someapi internal err:\n${err.stack}`.error);
   }
 };
 
 async function mockify(text) {
   if (!text) {
-    console.log("\x1b[31m", "  > someapi input err: please provide some text!");
+    console.log("  > someapi input warning: please provide some text!".warn);
     process.exit();
   }
   try {
@@ -90,7 +105,7 @@ async function mockify(text) {
     const dataPromise = promise.then((response) => response.data.text);
     return dataPromise;
   } catch (err) {
-    console.log("\x1b[31m", `  > someapi internal err:\n${err.stack}`);
+    console.log(`  > someapi internal err:\n${err.stack}`.warn);
   }
 };
 
